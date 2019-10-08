9f102bddd3903159d70c92a64b7a49208248c037
Hendrik Luehrsen <Luehrsen@users.noreply.github.com>
2019-10-08 09:56:39 +0200

    Modify the defaults to more closely align to the theme-boilerplate (#4)
    * The testVersion here is overwritten by rulesets referenced further down
    
    * Rename the main file
    
    "-plugin" slugs are forbidden in WP Repo and this file should be named like the main slug
    
    * The slug should not be %slug%-plugin
    
    * Rename the CamelCase variant to make it clearer
    
    * CamelCase Variable
    
    * Update README.md
    
    * Add an override ruleset to create config with higher hierachy
    
    * Add a plugin readme
    
    * Create CONTRIBUTING.md
    
    * Rename template tags to plugin functions
    
    fixes #3
    
    * Update composer.json
    
    * The testVersion comes from the override ruleset
    
    * Update package-lock.json
    
    * Fix some Constants
    
    * Align Readme
    
    * Remove the rename script, as it does work anymore with current names
    
    * Fixed some readme

--- build/inc/Templating_Component.php
+++ build/inc/Templating_Component.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * _Lhpbp\Templating_Component_Interface interface
+ * _Lhpbp\Plugin_Component_Interface interface
  *
  * @package _lhpbp
  */
@@ -10,13 +10,13 @@ namespace _lhpbp;
 /**
  * Interface for a plugin component that exposes template tags.
  */
-interface Templating_Component_Interface {
+interface Plugin_Component_Interface {
 	/**
-	 * Gets template tags to expose as methods on the Template_Tags class instance, accessible through `wp__lhpbp()`.
+	 * Gets template tags to expose as methods on the Plugin_Functions class instance, accessible through `wp__lhpbp()`.
 	 *
 	 * @return array Associative array of $method_name => $callback_info pairs. Each $callback_info must either be
 	 *               a callable or an array with key 'callable'. This approach is used to reserve the possibility of
 	 *               adding support for further arguments in the future.
 	 */
-	public function template_tags();
+	public function plugin_functions();
 }
